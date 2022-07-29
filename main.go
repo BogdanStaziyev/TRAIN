07613653d206b0ab04d563a8482422c425e902e1
BogdanStaziyev <94914224+BogdanStaziyev@users.noreply.github.com>
2022-07-29 21:18:42 +0300

    "read from json"

--- main.go
+++ main.go
@@ -1,19 +1,51 @@
 package main
 
-import "time"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"time"
+)
 
-type Trains []Train
+type TrainsDTO []TrainDTO
+
+type TrainDTO struct {
+	TrainID            int     `json:"trainId"`
+	DepartureStationID int     `json:"departureStationId"`
+	ArrivalStationID   int     `json:"arrivalStationId"`
+	Price              float32 `json:"price"`
+	ArrivalTime        string  `json:"arrivalTime"`
+	DepartureTime      string  `json:"departureTime"`
+}
+
+type Trains []TrainDTO
 
 type Train struct {
-	TrainID            int
-	DepartureStationID int
-	ArrivalStationID   int
-	Price              float32
-	ArrivalTime        time.Time
-	DepartureTime      time.Time
+	TrainID            int       `json:"trainId"`
+	DepartureStationID int       `json:"departureStationId"`
+	ArrivalStationID   int       `json:"arrivalStationId"`
+	Price              float32   `json:"price"`
+	ArrivalTime        time.Time `json:"arrivalTime"`
+	DepartureTime      time.Time `json:"departureTime"`
 }
 
 func main() {
+	sliseBite, err := ioutil.ReadFile("data.json")
+	if err != nil {
+		log.Println("error read all")
+	}
+	b := &TrainsDTO{}
+	err = json.Unmarshal(sliseBite, &b)
+	if err != nil {
+		log.Println("error Unmarshal")
+	}
+	for _, val := range *b {
+		if val.TrainID == 8801 {
+			fmt.Println(val)
+		}
+	}
+
 	//	... запит даних від користувача
 	//result, err := FindTrains(departureStation, arrivalStation, criteria))
 	//	... обробка помилки
